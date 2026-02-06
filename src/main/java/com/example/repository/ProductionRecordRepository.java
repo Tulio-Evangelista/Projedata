d0265456c1c3f81a52c9c4eeeb3b3e68feb7610a
SiroiluT <tulio-evangelista@outlook.com>
2026-02-06 04:22:08 -0300

    refactor: "Reorganize backend folder and add frontend"

--- src/main/java/com/example/repository/ProductionRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.ProductionRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductionRecordRepository extends JpaRepository<ProductionRecord, Long> {
-}
